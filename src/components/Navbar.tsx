5f5147f0a1e5968082a0d343f6e8a30ed9ccd535
ChukwukaHonour <ChukwukaHonour@gmail.com>
2023-12-13 14:45:33 +0100

    Added the register link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,8 @@
 import Link from 'next/link'
 import MaxWidthWrapper from './MaxWidthWrapper'
 import { buttonVariants } from './ui/button'
-import { LoginLink } from '@kinde-oss/kinde-auth-nextjs/server'
+import { LoginLink, RegisterLink } from '@kinde-oss/kinde-auth-nextjs/server'
+import { ArrowRight } from 'lucide-react'
 
 
 const Navbar = async () => {
@@ -24,6 +25,12 @@ const Navbar = async () => {
                   variant: "ghost",
                   size: 'sm'
                 })} >Sign in</LoginLink>
+                <RegisterLink className={buttonVariants({
+                  size: 'sm'
+                })}>
+                  Register
+                  <ArrowRight className='ml-1.5 h-5 w-5' />
+                </RegisterLink>
               </>
             </div>
           </div>
